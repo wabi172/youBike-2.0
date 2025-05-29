63832c8abe72a5220c8de324739edadb642dc702
wabi172 <114135493+wabi172@users.noreply.github.com>
2025-05-29 10:59:20 +0800

    U

--- next-vc-main/hooks/use-data.js
+++ next-vc-main/hooks/use-data.js
@@ -1,17 +1,19 @@
 import React, { useEffect, useState } from 'react'
+import { apiURL } from '@/config'
 
 export default function useData() {
   const [data, setData] = useState(null);
   const [loading,setLoading] = useState(false);
   const [error, setError] = useState(null)
-  const url = 'http://localhost:3005/proxy'
+  // const url = 'http://localhost:3005/proxy'
+  // 有設置apiURL可以替換
 
   useEffect(()=>{
   const fetchData = async () => {
     setLoading(true)
 
     try {
-      const res = await fetch(url)
+      const res = await fetch(apiURL)
          if (!res.ok) throw Error(`Fetch error!!`);
       const result = await res.json()
       setData(result)
